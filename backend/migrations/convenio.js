410a29f04839ce6d9726fd788d8c1c8b4e4add64
Lizzandra Alves <168601460+Chiquinhosorvetes@users.noreply.github.com>
2025-09-01 20:06:54 -0300

    migrations convenio

--- backend/migrations/convenio.js
+++ backend/migrations/convenio.js
@@ -1,3 +1,29 @@
+'use strict';
 
+Module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('convenios', {
+            id: {
+                type: Sequelize.UUID,
+                primaryKey: true,
+                defaultValue: Sequelize.literal('gen_random_uuid()'),
+                allowNull: false,
+            },
+            nome: {
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('NOW()'),
+                allowNull: false,
+            },
+            updated: {
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('NOW()'),
+                allowNull: false,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('convenios');
+    }
+};
 
 
