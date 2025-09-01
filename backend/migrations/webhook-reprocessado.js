728b3fd185d10e438dcd3717073301626a9c2dc7
Lizzandra Alves <168601460+Chiquinhosorvetes@users.noreply.github.com>
2025-09-01 20:49:06 -0300

    migrations webhook

--- backend/migrations/webhook-reprocessado.js
+++ backend/migrations/webhook-reprocessado.js
@@ -1,3 +1,56 @@
+'use strict';
 
+const servico = require("./servico");
+
+Module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('webhook_reprocessados',{
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.literal('gen_random_uuid()'),
+                primaryKey: true,
+                allowNull: false,
+            },
+            data: {
+                type: Sequelize.JSONB, //PostgreSQL
+                allowNull: false,
+            },
+            data_criacao: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal('NOW()'),
+            },
+            cedente_id: {
+                type: Sequelize.UUID,
+                allowNull: false,
+                references: {
+                    model: 'cedentes',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE'
+            },    
+                kind: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                type: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                servico_id: {
+                    type: Sequelize.TEXT, //armazena array de IDs como JSON
+                    allowNull: false,
+                },
+                protocolo: {
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('webhook_reprocessados');
+    }
+};
 
 
