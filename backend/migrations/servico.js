0d49fac88c762424ca20f8ae4ec895821e364115
Lizzandra Alves <168601460+Chiquinhosorvetes@users.noreply.github.com>
2025-09-01 20:18:29 -0300

    migrations serviços

--- backend/migrations/servico.js
+++ backend/migrations/servico.js
@@ -1,3 +1,39 @@
+'use strict'
 
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('servicos', {
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.literal('gen_random_uuid()'),
+                primaryKey: true,
+                allowNull: false,
+            },
+            data_criacao: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal('NOW()'),
+            },
+            convenio_id: {
+                type: Sequelize.UUID,
+                allowNull: false,
+                references: {
+                    model: 'convenios',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            },
+            status:{
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('servicos');
+    }
+};
 
 
