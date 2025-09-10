14015aed231d0d448feda127dc6461c06815b1dd
Lizzandra Alves <168601460+Chiquinhosorvetes@users.noreply.github.com>
2025-09-09 22:08:11 -0300

    Ajustando as migrations

--- backend/migrations/cedente.js
+++ backend/migrations/cedente.js
@@ -2,38 +2,38 @@
 
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('cedentes', {
-      id: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        primaryKey: true,
-        defaultValue: Sequelize.literal('gen_random_uuid()')
-      },
-      nome: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.fn('NOW')
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.fn('NOW')
-      }
-    });
-  },
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cedentes', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.literal('gen_random_uuid()')
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW')
+      }
+    });
+  },
 
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('cedentes');
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('cedentes');
+  }
 };
--- backend/migrations/convenio.js
+++ backend/migrations/convenio.js
@@ -2,33 +2,33 @@
 
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('convenios', {
-      id: {
-        type: Sequelize.UUID,
-        primaryKey: true,
-        defaultValue: Sequelize.literal('gen_random_uuid()'),
-        allowNull: false,
-      },
-      nome: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('NOW()'),
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('NOW()'),
-        allowNull: false,
-      },
-    });
-  },
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('convenios', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.literal('gen_random_uuid()'),
+        allowNull: false,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()'),
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()'),
+        allowNull: false,
+      },
+    });
+  },
 
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('convenios');
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('convenios');
+  }
 };
