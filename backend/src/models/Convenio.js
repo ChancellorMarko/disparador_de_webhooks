95dc17ec3df3c4d96fa2130a48b723358491d5ff
Lizzandra Alves <168601460+Chiquinhosorvetes@users.noreply.github.com>
2025-09-09 22:16:33 -0300

    Subindo a models (em fase de verificação)

--- backend/src/models/Convenio.js
+++ backend/src/models/Convenio.js
@@ -1,3 +1,42 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Convenio = sequelize.define('Convenio', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: sequelize.literal('gen_random_uuid()'),
+      allowNull: false
+    },
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('NOW()'),
+      allowNull: false
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('NOW()'),
+      allowNull: false
+    }
+  }, {
+    tableName: 'convenios',
+    underscored: true,
+    timestamps: false
+  });
+
+  Convenio.associate = function(models) {
+    Convenio.hasMany(models.Servico, {
+      foreignKey: 'convenio_id',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    });
+  };
+
+  return Convenio;
+};
 
 
 
