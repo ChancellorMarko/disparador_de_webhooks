eb2ba289c19cbe1bb1ed18657c1c61dd8a4a4fe6
Joao Antonio <joao.souza.76@outlook.com>
2025-10-02 20:01:24 -0300

    texto de verificação da coneção do banco

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,14 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+console.log('Conectando com:', {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
+
 const sequelize = new Sequelize({
   dialect: 'postgres',
   host: process.env.DB_HOST || 'localhost',
